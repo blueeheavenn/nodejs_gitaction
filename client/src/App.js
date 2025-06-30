403016c2a17c7e710e71fc8b015b96ed97ad3fc5
blueeheavenn <rajib.ahmed.chi@gmail.com>
2025-06-30 13:58:51 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Just react starter, cmon start vercel now</h1>
+          <h1 className="App-title">Just react starter, cmon start vercel now-last time</h1>
         </header>
         <Customers />
       </div>
